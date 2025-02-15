d50bb51f514930566cb0f5e0868a1b4a6c9481e1
Mithul Mathivanan <mithulmathi@gmail.com>
2025-02-15 15:08:09 -0800

    Player now has water health. First load is still buggy, max health update needs to be tweaked

--- scripts/water_drops.gd
+++ scripts/water_drops.gd
@@ -4,10 +4,14 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
-
+@export var health: float = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	
+	for child in body.get_children():
+		if child is HealthController:
+			child.healed(health)
 	queue_free()
 	animation_player.play("pickup")
 	#print("coin collected")
--- scripts/water_ui.gd
+++ scripts/water_ui.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var water_ui_empty: TextureRect = $WaterUIEmpty
 @onready var water_ui_full: TextureRect = $WaterUIFull
 
-var max_water: float = 0.0: set = _set_max_water, get = _get_max_water
+var max_water: float = 0: set = _set_max_water, get = _get_max_water
 var water: float = 0.0: set = _set_water, get = _get_water
 
 func _ready() -> void:
@@ -14,25 +14,27 @@ func _ready() -> void:
 	EventController.emit_signal("on_health_ui_ready", player)
 	
 func _set_water(value:float) -> void:
-		water = lamp(value, 0, max_water)
-		if water_ui_full != null:
-			water_ui_full.set_size(Vector2(value * 32, 32))
+	print("set "+str(value))
+	water = clamp(value, 0, max_water)
+	if water_ui_full != null and max_water != 0:
+		water_ui_full.set_size(Vector2(value*365, 365))
+		print(water_ui_full.size)
 			
 func _get_water() -> float:
 	return water
-	
-	
-	
+
 func _set_max_water(value:float) ->void:
 	max_water = max(value, 1)
 	if water_ui_empty !=null:
 		water_ui_empty.set_size(Vector2(value * 32, 32))
-		
-		
+
 func _get_max_water() -> float:
 	return max_water
-	
-	
-	
-func on_event_health_changed(node: Node, new_health: float):
-	
+
+func on_event_max_health_changed(node: Node, new_max_water: float):
+	if node == player:
+		max_water = new_max_water
+
+func on_event_health_changed(node: Node, new_water: float):
+	if node == player:
+		water = new_water
